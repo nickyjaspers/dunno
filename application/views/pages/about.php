d1dfdb7bae1f123db333fbdab4b902fb0e90e091
Nicky Jaspers <nicky_jaspers@hotmail.com>
2013-01-26 16:07:13 +0100

    made some modifications on menu styles :

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,1 +1,38 @@
 about
+
+<div id="MenuItemSelectedWrapper">
+	<div id="MenuItemSelected">
+		<div class="MenuItemSelectedItem">
+			One
+		</div>
+
+		<div class="MenuItemSelectedItem">
+			Two
+		</div>
+
+		<div class="MenuItemSelectedItem">
+			Three
+		</div>
+
+		<div class="MenuItemSelectedItem">
+			Four
+		</div>
+		
+		<div class="MenuItemSelectedItem">
+			Five
+		</div>	
+		
+		<div class="MenuItemSelectedItem">
+			Six
+		</div>	
+	</div>
+	
+	<div id="MenuItemSelectContent">
+		bla
+		bla
+		bla
+		</br>
+		bla
+		
+	</div>	
+</div>
